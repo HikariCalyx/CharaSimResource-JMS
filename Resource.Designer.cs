f19b068d1a3a182386c301da08caaa70d9fc0e9a
seotbeo <yoonv72@gmail.com>
2025-07-03 19:15:53 +0900

    Add 6th ascent skill icon

--- Resource.Designer.cs
+++ Resource.Designer.cs
@@ -6053,6 +6053,16 @@ namespace CharaSimResource {
         /// <summary>
         ///   System.Drawing.Bitmap 형식의 지역화된 리소스를 찾습니다.
         /// </summary>
+        public static System.Drawing.Bitmap UIWindow2_img_Skill_skillTypeIcon_ascent {
+            get {
+                object obj = ResourceManager.GetObject("UIWindow2_img_Skill_skillTypeIcon_ascent", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   System.Drawing.Bitmap 형식의 지역화된 리소스를 찾습니다.
+        /// </summary>
         public static System.Drawing.Bitmap UIWindow2_img_Skill_skillTypeIcon_origin {
             get {
                 object obj = ResourceManager.GetObject("UIWindow2_img_Skill_skillTypeIcon_origin", resourceCulture);
